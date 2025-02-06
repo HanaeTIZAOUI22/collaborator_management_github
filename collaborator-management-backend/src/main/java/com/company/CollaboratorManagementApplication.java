9d432df707195c59670365613dd000ed04dd58ba
HANAE TIZAOUI <195500991+HanaeTIZAOUI22@users.noreply.github.com>
2025-02-06 17:46:32 +0100

    Set up CI with Azure Pipelines
    [skip ci]

---
+++ collaborator-management-backend/src/main/java/com/company/CollaboratorManagementApplication.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaboratorManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CollaboratorManagementApplication.class, args);
+    }
+}
